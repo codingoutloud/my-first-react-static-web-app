b8b1c2a9e763edd84f108cecdc47557c38ef9d30
Bill Wilder <codingoutloud@gmail.com>
2022-11-16 18:43:12 -0500

    unbreak

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   var date = new Date();
   const clock = '⏰';
   // blind spot for internationalization - can we detect in a Playwright test that uses different browser settings?
-  return <div>Right now is {date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} @ {date.getHours()}:{date.getMinutes()}:{date.getSeconds()} {emoji}</div>;
+  return <div>Right now is {date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} @ {date.getHours()}:{date.getMinutes()}:{date.getSeconds()} {clock}</div>;
 }
 
 export default App;
