0b0ae44d330d880c93da08d33ad2c31100b86adb
Bill Wilder <codingoutloud@gmail.com>
2022-11-16 18:30:42 -0500

    Be sloppy with date format

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  var date = new Date();
+
+  // blind spot for internationalization - can we detect in a Playwright test that uses different browser settings?
+  return <div>Today is {date.getFullYear()}-{date.getMonth()+1}-{date.getDate()}</div>;
 }
 
 export default App;
