f0297e0342e613dcab8950b743a6e1e08448ba19
Bill Wilder <codingoutloud@gmail.com>
2022-11-16 20:40:04 -0500

    one in 4

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
   const clock = '⏰';
   var date = new Date();
   
-  var x = 1 / (date.getSeconds() % 4 - 3); // on time in 4 will cause divide by zero error
+  var x = 1 / ((date.getSeconds() % 4) - 3); // on time in 4 will cause divide by zero error
   
   // blind spot for internationalization - can we detect in a Playwright test that uses different browser settings?
-  return <div>Hello World! Right now is {date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} @ {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}! {clock}</div>;
+  return <div>Hello World! {x} Right now is {date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} @ {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}! {clock}</div>;
 }
 
 export default App;
