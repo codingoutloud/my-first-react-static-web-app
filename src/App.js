b0bb7a58827c4f0043a770d30aefe4e7f27e6749
Bill Wilder <codingoutloud@gmail.com>
2022-11-17 14:37:04 -0500

    n

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ function App() {
   var date = new Date();
   
   const failEveryNSeconds = 4;
-  if (date.getSeconds() % failEveryNSeconds === 0)
-    throw new Error('Something "bad" happened (1 of every {failEveryNSeconds} seconds}).');
+  let n = date.getSeconds();
+  if (n % failEveryNSeconds === 0)
+    throw new Error(`Something "bad" happened (1 of every {failEveryNSeconds} seconds}).`);
   
   // blind spot for internationalization - can we detect in a Playwright test that uses different browser settings?
-  return <div>Hello World! [{failEveryNSeconds}] Right now is {date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} @ {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}! {clock}</div>;
+  return <div>Hello World! [{n}/{failEveryNSeconds}] Right now is {date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} @ {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}! {clock}</div>;
 }
 
 export default App;
