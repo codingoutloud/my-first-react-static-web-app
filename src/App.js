8314c796021ea7e21c70a6cece48ca951f37d65e
Bill Wilder <codingoutloud@gmail.com>
2022-11-16 20:35:16 -0500

    Sometimes divide by zero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 function App() {
-  var date = new Date();
   const clock = '⏰';
+  var date = new Date();
+  
+  var x = 1 / (date.getSeconds() % 4 - 3); // on time in 4 will cause divide by zero error
+  
   // blind spot for internationalization - can we detect in a Playwright test that uses different browser settings?
-  return <div>Hello World! Right now is {date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} @ {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}! {clockchar}</div>;
+  return <div>Hello World! Right now is {date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} @ {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}! {clock}</div>;
 }
 
 export default App;
